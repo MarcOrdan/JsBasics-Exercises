2806e15167142eb02399f073188eab345f599cbb
MarcOrdan <marcordan@gmail.com>
2019-04-19 20:17:18 +0800

    Speed checker code with points system

--- SpeedCheckWithDemeritSystem.js
+++ SpeedCheckWithDemeritSystem.js
@@ -1,9 +1,24 @@
 //speed limit  = 70
+//for every 5kms above the speed limit, you will get 1 pt.
+//if points > 12 => return Licensed suspended
 
+//use math.floor
+checkSpeed(130);
 
 function checkSpeed(speed){
-  if (speed === 70) {
+   const speedLimit = 70;
+   const kmPerPoint= 5;
+
+  if (speed < speedLimit + kmPerPoint)  {
+    console.log('Ok');
+  } else {
+      const testForPoints = Math.floor((speed-speedLimit)/kmPerPoint)
+      if (testForPoints >= 12){
+           console.log('LICENSED SUSPENDED');
+      } else {
+            console.log('Point(s):', testForPoints) ;
+            }
 
   }
-  
+
 }
